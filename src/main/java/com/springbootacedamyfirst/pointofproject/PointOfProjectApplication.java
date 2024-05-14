806e80ccb3ba31eb1b4127fea726aad0c2fa7dcd
rlakmal <rruchiralakmal@gmail.com>
2024-05-15 00:04:19 +0530

    initial commmit

---
+++ src/main/java/com/springbootacedamyfirst/pointofproject/PointOfProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springbootacedamyfirst.pointofproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfProjectApplication.class, args);
+	}
+
+}
